985ea9b75b1b8f88ebadcf5ca3fc45d412102e18
GenieGenerate <geniegenerate@gmail.com>
2024-06-06 14:10:33 +0700

    Ep4 - API version

---
+++ routes/api_v1.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\Api\V1\TicketController;
+use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// http://localhost:8000/api/
+// Universal Resource Locator (URL)
+// tickets
+// users
+
+Route::apiResource('tickets', TicketController::class);
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
