d39f94d9646a190783a01c5e9a0edb204feeb0bb
GenieGenerate <geniegenerate@gmail.com>
2024-06-06 15:43:23 +0700

    EP 5 - Authentication using token

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\AuthController;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::get('/tickets', function(){
+Route::middleware(['auth:sanctum'])->get('/tickets', function(){
     return Ticket::all();
 });
 
