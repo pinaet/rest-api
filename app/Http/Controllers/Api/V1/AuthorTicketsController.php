02fe98674166655dca0e99d05689c75525c6558b
GenieGenerate <geniegenerate@gmail.com>
2024-06-09 16:27:51 +0700

    EP 13 - Creating resources with Post requests

--- app/Http/Controllers/Api/V1/AuthorTicketsController.php
+++ app/Http/Controllers/Api/V1/AuthorTicketsController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Models\Ticket;
 use App\Http\Controllers\Controller;
 use App\Http\Filters\V1\TicketFilter;
 use App\Http\Resources\V1\TicketResource;
-use App\Models\Ticket;
+use App\Http\Requests\Api\V1\StoreTicketRequest;
 
 class AuthorTicketsController extends Controller
 {
@@ -14,5 +15,20 @@ class AuthorTicketsController extends Controller
         return TicketResource::collection(
             Ticket::where('user_id', $author_id)->filter($filters)->paginate()
         );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store($author_id, StoreTicketRequest $request)
+    {
+        $model = [
+            'title'       => $request->input('data.attributes.title'),
+            'description' => $request->input('data.attributes.description'),
+            'status'      => $request->input('data.attributes.status'),
+            'user_id'     => $author_id,
+        ];
+
+        return new TicketResource(Ticket::create($model));
     }
 }
