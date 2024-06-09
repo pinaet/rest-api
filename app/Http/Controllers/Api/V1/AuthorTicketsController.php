c48ce96c7fc62ed17de48df25f2c2f8d1daef288
GenieGenerate <geniegenerate@gmail.com>
2024-06-09 19:05:50 +0700

    EP 15 - Replacing resources with put request

--- app/Http/Controllers/Api/V1/AuthorTicketsController.php
+++ app/Http/Controllers/Api/V1/AuthorTicketsController.php
@@ -6,6 +6,7 @@ use App\Models\Ticket;
 use App\Http\Filters\V1\TicketFilter;
 use App\Http\Resources\V1\TicketResource;
 use App\Http\Requests\Api\V1\StoreTicketRequest;
+use App\Http\Requests\Api\V1\ReplaceTicketRequest;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class AuthorTicketsController extends ApiController
@@ -30,6 +31,31 @@ class AuthorTicketsController extends ApiController
         ];
 
         return new TicketResource(Ticket::create($model));
+    }
+
+    public function replace(ReplaceTicketRequest $request, $author_id, $ticket_id)
+    {
+        // PUT
+        try {
+            $ticket = Ticket::findOrFail($ticket_id);
+
+            if($ticket->user_id == $author_id){
+
+                $model = [
+                    'title'       => $request->input('data.attributes.title'),
+                    'description' => $request->input('data.attributes.description'),
+                    'status'      => $request->input('data.attributes.status'),
+                    'user_id'     => $author_id,
+                ];
+
+                $ticket->update($model);
+                return new TicketResource($ticket);
+            }
+            // TODO: ticket does not belong to user
+            return $this->error('ticket does not belong to user', 404);
+        } catch (ModelNotFoundException $exception) {
+            return $this->error('Ticket cannot be found', 404);
+        }
     }
 
     /**
